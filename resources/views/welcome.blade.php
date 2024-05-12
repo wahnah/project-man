53c893d7b30e91d5e6d048a0a1bd4a1234c3e794
wahnah <wanamuz05@gmail.com>
2024-05-12 22:00:54 +0200

    modified:   resources/views/components/application-logo.blade.php modified:   resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,16 @@
 <x-app-layout>
-    <div class="flex items-center justify-center" style="background: #edf2f7;">
+    <div class="flex items-center justify-center">
 
-        <section class="relative  bg-blueGray-50">
-            <div class="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
-                <div class="absolute top-0 w-full h-full bg-center bg-cover"
-                    style="
-                background-image: url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1267&amp;q=80');
-              ">
-                    <span id="blackOverlay" class="w-full h-full absolute opacity-75 bg-black"></span>
-                </div>
-                <div class="container relative mx-auto">
+
+
+                <div class="container">
                     <div class="items-center flex flex-wrap">
                         <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
                             <div class="pr-12">
-                                <h1 class="text-white font-semibold text-5xl">
-                                    {{ __('Project Management Application') }}
+                                <h1 class="text-amber-500 font-semibold text-5xl">
+                                    {{ __('Project Tracker') }}
                                 </h1>
-                                <p class="mt-4 text-xl text-blueGray-200">
+                                <p class="mt-4 text-xl text-black-200">
                                     {{ __('Easy, fast, efficient.') }}
                                 </p>
                             </div>
@@ -30,67 +24,7 @@
                         <polygon class="text-blueGray-200 fill-current" points="2560 0 2560 100 0 100"></polygon>
                     </svg>
                 </div>
-            </div>
-            <section class="pb-10 bg-blueGray-200 -mt-24">
-                <div class="container mx-auto px-4">
-                    <div class="flex flex-wrap">
-                        <x-welcome.data-section class="lg:pt-12 pt-6" icon-bg="bg-amber-400">
-                            <x-slot:title>
-                                {{ __('Projects') }}
-                            </x-slot>
-                            <x-slot:description>
-                                {{ __('In the Projects section, you can view and manage active projects. Add new projects, edit information, and track the status of each project.') }}
-                            </x-slot>
-                            <x-slot:icon>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
-                                </svg>
-                            </x-slot>
-                        </x-welcome.data-section>
-                        <x-welcome.data-section icon-bg="bg-lightBlue-400">
-                            <x-slot:title>
-                                {{ __('Tasks') }}
-                            </x-slot>
-                            <x-slot:description>
-                                {{ __('The Tasks section provides a convenient way to view and manage your workload. Create new tasks, assign them to employees, and track their progress.') }}
-                            </x-slot>
-                            <x-slot:icon>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
-                                </svg>
-                            </x-slot>
-                        </x-welcome.data-section>
-                        <x-welcome.data-section icon-bg="bg-green-400" class="pt-6">
-                            <x-slot:title>
-                                {{ __('Employees and Teams') }}
-                            </x-slot>
-                            <x-slot:description>
-                                {{ __('In the Employees and Teams sections, you can view a list of employees, their positions, and assigned tasks. Add new employees and form working groups for effective project management.') }}
-                            </x-slot>
-                            <x-slot:icon>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
-                                </svg>
-                            </x-slot>
-                        </x-welcome.data-section>
-                    </div>
 
-                    <footer class="relative  pt-8 pb-6 mt-1">
-                        <div class="container mx-auto px-4">
-                            <div class="flex flex-wrap items-center md:justify-between justify-center">
-                                <div class="w-full md:w-6/12 px-4 mx-auto text-center">
-                                </div>
-                            </div>
-                        </div>
-                    </footer>
-            </section>
-        </section>
     </div>
 
 
